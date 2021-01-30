1ea82963e3ba242aa8becf726a72fac84f7e9b0e
Daniel Schwensen <daniel.schwensen@gmail.com>
2021-01-30 11:33:25 +0100

    Added 06_JavaScript

---
+++ 06_JavaScript/dog2HumanAge.js
@@ -0,0 +1,7 @@
+// Formular = humanAge = (dogAge -2) x 4 + 21
+
+
+var humanAge = 0;
+var dogAge = prompt("What's the age of your dog?");
+humanAge = ((dogAge -2) * 4) + 21 ;
+alert(`Your dog is ${humanAge} years old.`);
