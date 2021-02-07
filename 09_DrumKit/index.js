d5d81524ed58225517765f1588c2c673cca7b1d1
Daniel Schwensen <daniel.schwensen@gmail.com>
2021-02-07 15:01:22 +0100

    Added event listener to all drum buttons

--- 09_DrumKit/index.js
+++ 09_DrumKit/index.js
@@ -1,6 +1,16 @@
-
-document.querySelector("button").addEventListener("click",  handleClick);
-
-function handleClick() {
+/*
+document.querySelector("button").addEventListener("click", function () {
     alert("I got clicked!")
+});
+*/
+
+// Add click event to all buttons
+var classname = document.getElementsByClassName("drum");
+
+var clickButtons = function(){
+    alert("I got clicked!")
+};
+
+for (var i = 0; i < classname.length; i++) {
+    classname[i].addEventListener('click', clickButtons);
 }
