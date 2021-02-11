13d24ebd5fe7ff72e4fb49c9893e71184f4a66b3
Daniel Schwensen <daniel.schwensen@gmail.com>
2021-02-11 09:24:50 +0100

    Added fadeOut adn sound

--- 11_SimonGame/game.js
+++ 11_SimonGame/game.js
@@ -1,7 +1,18 @@
+var pathToSounds = "../11_SimonGame/sounds/";
+var gamePattern = [];
+
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4 );
     var randomChosenColour = buttonColours[randomNumber];
-    return randomChosenColour;
+    //return randomChosenColour;
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+    var playSound = new Audio( pathToSounds + randomChosenColour + ".mp3" );
+    playSound.play();
+    gamePattern.push(randomChosenColour);
 }
+
+//Blink
+//$(" #randomChosenColour ").fadeOut(100).fadeIn(100);
+
