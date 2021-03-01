410301e0e08c4a5a30135278ac62da9e55946191
Daniel Schwensen <daniel.schwensen@gmail.com>
2021-03-01 08:43:43 +0100

    Button click detection

--- 11_SimonGame/game.js
+++ 11_SimonGame/game.js
@@ -1,5 +1,6 @@
 var pathToSounds = "../11_SimonGame/sounds/";
 var gamePattern = [];
+var userClickedPattern = [];
 
 var buttonColours = ["red", "blue", "green", "yellow"];
 
@@ -8,10 +9,25 @@ function nextSequence() {
     var randomChosenColour = buttonColours[randomNumber];
     //return randomChosenColour;
     $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
-    var playSound = new Audio( pathToSounds + randomChosenColour + ".mp3" );
-    playSound.play();
+    
+    playSound(randomChosenColour);
     gamePattern.push(randomChosenColour);
 }
 
 //Blink
 //$(" #randomChosenColour ").fadeOut(100).fadeIn(100);
+
+// Play playSound
+function playSound(name) {
+    var audio = new Audio( pathToSounds + name + ".mp3" );
+    audio.play();
+}
+
+//Button click detection
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  //console.log(userClickedPattern);
+
+});
