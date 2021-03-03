b5dd5a5703a8bff869aea2d8612877dc7897e14d
Daniel Schwensen <daniel.schwensen@gmail.com>
2021-03-03 09:06:43 +0100

    final touch

--- 11_SimonGame/game.js
+++ 11_SimonGame/game.js
@@ -6,27 +6,15 @@ var started = false;
 
 var buttonColours = ["red", "blue", "green", "yellow"];
 
-function nextSequence() {
-    var randomNumber = Math.floor(Math.random() * 4 );
-    var randomChosenColour = buttonColours[randomNumber];
-    //return randomChosenColour;
-    $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
-    
-    playSound(randomChosenColour);
-    gamePattern.push(randomChosenColour);
+$(document).one("keypress", function (e) {
+  if (!started) {
 
-    level++;
     $("#level-title").text("Level " + level);
-}
+    nextSequence();
+    started = true;
+  }
 
-//Blink
-//$(" #randomChosenColour ").fadeOut(100).fadeIn(100);
-
-// Play playSound
-function playSound(name) {
-    var audio = new Audio( pathToSounds + name + ".mp3" );
-    audio.play();
-}
+});
 
 //Button click detection
 $(".btn").click(function() {
@@ -41,40 +29,40 @@ $(".btn").click(function() {
 
 });
 
-
 function checkAnswer(currentLevel) {
-
-
   if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-
     console.log("success");
-
-
     if (userClickedPattern.length === gamePattern.length){
-
-
       setTimeout(function () {
         nextSequence();
       }, 1000);
-
     }
-
   } else {
-
     console.log("wrong");
     playSound("wrong");
-
     $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
     setTimeout(function () {
       $("body").removeClass("game-over");
     }, 200);
 
-    $("#level-title").text("Game Over, Press Any Key to Restart");
-
     startOver();
-
   }
 
+}
+
+function nextSequence() {
+
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4 );
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
 }
 
 function animatePress(currentColor) {
@@ -86,15 +74,11 @@ function animatePress(currentColor) {
   }, 100);
 }
 
-$(document).one("keypress", function (e) {
-  if (!started) {
-
-    $("#level-title").text("Level " + level);
-    nextSequence();
-    started = true;
-  }
-
-});
+// Play playSound
+function playSound(name) {
+  var audio = new Audio( pathToSounds + name + ".mp3" );
+  audio.play();
+}
 
 function startOver() {
   level = 0;
