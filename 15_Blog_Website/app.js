f8dace3514acbbaf8b61dc2502935a197d71d7e9
Daniel Schwensen <daniel.schwensen@gmail.com>
2021-03-06 13:28:22 +0100

    added compose page

--- 15_Blog_Website/app.js
+++ 15_Blog_Website/app.js
@@ -30,7 +30,10 @@ app.get( "/contact", function ( req,  res ) {
   res.render("contact", { contact: contactContent });
 });
 
+app.get( "/compose", function ( req,  res ) {
 
+  res.render("compose", { contact: contactContent });
+});
 
 
 
